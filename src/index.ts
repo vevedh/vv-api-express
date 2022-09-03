a919d6e8015b9a2189620f439b0bcd35611cc7c5
Hervé de CHAVIGNY <vevedh@gmail.com>
2022-09-03 15:34:02 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { config } from '~/config'
 import { PetsController } from '~/resources/pets/pets.controller'
 import { ExceptionsHandler } from '~/middlewares/exceptions.handler'
 import { UnknownRoutesHandler } from '~/middlewares/unknownRoutes.handler'
+import { JsomNode } from "sp-jsom-node"
 
 /**
  * On créé une nouvelle "application" express
@@ -31,7 +32,47 @@ app.use('/pets', PetsController)
 /**
  * Homepage (uniquement necessaire pour cette demo)
  */
-app.get('/', (req, res) => res.send('🏠'))
+app.post('/lists', async (req, res) => {
+      const siteUrl = req.body.siteUrl
+      const username = req.body.username
+      const password = req.body.password
+
+      const domain = req.body.domain
+
+      console.log("Site url :",siteUrl)
+      console.log("username :",username)
+      console.log("password :",password)
+      console.log("domain :",domain)
+      const sp2019: JsomNode = new JsomNode({
+        modules: ["taxonomy", "userprofiles"],
+      })
+      const ctx = sp2019
+        .init({
+          siteUrl: siteUrl,
+          authOptions: {
+            username: username,
+            password: password,
+            domain: domain
+          }
+        })
+      const spctx = await ctx.getContext()
+      const oListsCollection: SP.ListCollection = spctx.get_web().get_lists()
+      spctx.load(oListsCollection, "Include(Title)")
+      await spctx.executeQueryPromise()
+      const arrList:any[] = []
+      const listsTitlesArr = oListsCollection.get_data().map(l => {
+	arrList.push({ Title:l.get_title() })
+	})
+      
+
+      console.log("List test :", arrList)
+      console.log("List :", listsTitlesArr)
+
+      console.log("TEST SHAREPOINT :", { result: "success!!!" })
+	
+     res.send(JSON.stringify(arrList)) 
+      //res.send('🏠')
+})
 
 /**
  * Pour toutes les autres routes non définies, on retourne une erreur
